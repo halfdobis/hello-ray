b90deb2a67e44638f0c2e569ae00e04cab7cfc00
missdobis <missdobis@gmail.com>
2020-11-05 00:26:00 +0100

    hello ray

---
+++ js/question.js
@@ -0,0 +1,33 @@
+// Array of Objects
+const quiz = [
+  {
+    q: "On the scale of 1 to 5, how much are you loving this?",
+    options: ["one", "two", "three", "four", "five"],
+    answer: 4,
+  },
+  {
+    q: "Which of these do you think is my full name?",
+    options: [
+      "ezenwa Adaobi Beauty",
+      "ezenwa Dobis Adaobi",
+      "ezenwa Adaobi Peace",
+      "ezenwa Adaobi Racheal",
+    ],
+    answer: 0,
+  },
+  {
+    q: "What am I passionate about?",
+    options: ["work", "tech", "music", "parties"],
+    answer: 1,
+  },
+  {
+    q: "Do I think that you a trouble maker but, fun?",
+    options: ["no", "yes", "hell, yes"],
+    answer: 2,
+  },
+  {
+    q: "I like you",
+    options: ["true", "false"],
+    answer: 0,
+  },
+];
